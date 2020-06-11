46e7c579ae88f33480433a2c4352b36d6bc560f5
carloshrf <carlos_6282010@hotmail.com>
2020-06-11 15:37:38 -0300

    continuing the backend

---
+++ backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -0,0 +1,23 @@
+import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderMonthAvailabilityService from '@modules/appointments/services/ListProviderMonthAvailabilityService';
+
+export default class ProviderMonthAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { month, year } = request.body;
+
+    const listProviderMonthAvailability = container.resolve(
+      ListProviderMonthAvailabilityService,
+    );
+
+    const providers = await listProviderMonthAvailability.execute({
+      month,
+      provider_id,
+      year,
+    });
+
+    return response.json(providers);
+  }
+}

43a951c1a85bd09dcbd3c6075a3c4de71dbc2a07
carloshrf <carlos_6282010@hotmail.com>
2020-07-11 09:48:06 -0300

    using class-transformer

--- backend/src/config/auth.ts
+++ backend/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '04e55d9fd62a045730a8a90af3e482da', // vaibarbeiro
+    secret: process.env.APP_SECRET || 'default', // vaibarbeiro
     expiresIn: '1d',
   },
 };

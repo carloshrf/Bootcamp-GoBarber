cd34c3e81fbc63190c8ac831b177baee9ba60b14
carloshrf <carlos_6282010@hotmail.com>
2020-08-10 22:27:52 -0300

    Realização de cache de appointments no dia

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,21 +1,50 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 import path from 'path';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
 
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName); // caso erro, retorna null, caso não, o nome do arquivo
+        return callback(null, fileName); // caso erro, retorna null, caso não, o nome do arquivo
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-2',
     },
-  }),
-};
+  },
+} as IUploadConfig;
